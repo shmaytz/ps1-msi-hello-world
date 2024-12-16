6a7c8591cbfa15a338c98a58bd6e94352009b2f0
shmaytz <120015172+shmaytz@users.noreply.github.com>
2024-12-16 17:13:52 +0200

    Add files via upload

--- MyScript.ps1
+++ MyScript.ps1
@@ -1,4 +1,5 @@
 $(
+#TEST
 # Define the module folder path
 # Get script directory using PSScriptRoot or fallback to MyInvocation
 $scriptDirectory = if ($PSScriptRoot) {
@@ -6,21 +7,17 @@ $scriptDirectory = if ($PSScriptRoot) {
 } else {
     Split-Path -Path $MyInvocation.MyCommand.Path -Parent
 }
-Write-Host "Script Directory: $scriptDirectory"
+Write-Output "Script Directory: $scriptDirectory"
 
 # Combine it with a relative path
 $modulePath = Join-Path -Path $scriptDirectory -ChildPath "Modules\MyScriptModule"
-Write-Host "Module Path: $modulePath"
+Write-Output "Module Path: $modulePath"
 
 # Import the module manifest file
 Import-Module "$modulePath\MyScript.psd1" -Force -ErrorAction Ignore
 
 # Verify the module is imported
-Write-Host "Module imported successfully!"
+Write-Output "Module imported successfully!"
 
 # Call the function from the module
-MyScript -DirectoryName "C:\TestDir" -Message Hello 
-) *> execution.log
-
-
-
+MyScript -DirectoryName "C:\TestDir" -Message "Silent Install" ) *> execution.log
